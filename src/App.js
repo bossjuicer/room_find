69efd172296396b940892a9808103e1ed33e453c
kashif <kashifmd123@gmail.com>
2021-08-20 00:45:18 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
+
+function Head(){
+  return (
+    <div>
+
+      {/* <h1>My name is kashif</h1>
+       */ }
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+}
+
+
+function Contain(){
+  return (
+    <>
+    <input></input>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header> */}
+        <Head />
+       </header>
+    
+      {/* <Contain/> */}
     </div>
   );
 }
